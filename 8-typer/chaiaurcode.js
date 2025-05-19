562dd0964a16f58174e9ce73ba19c22b3a907aa7
Nikhil Shaw <Nikhilshaw575@gmail.com>
2025-05-19 22:02:05 +0530

    Update chaiaurcode.js

--- 8-typer/chaiaurcode.js
+++ 8-typer/chaiaurcode.js
@@ -3,6 +3,35 @@ const cursor = document.querySelector('.cursor');
 
 const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
 
-function type() {}
+const typingDelay = 150;
+const erasingDelay = 150;
+const newTextDelay = 2000;
 
-function erase() {}
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < words[textArrayIndex].length) {
+    typedTextSpan.textContent += words[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    charIndex--; 
+    typedTextSpan.textContent = words[textArrayIndex].substring(0, charIndex);
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= words.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (words.length) setTimeout(type, newTextDelay + 250);
+});

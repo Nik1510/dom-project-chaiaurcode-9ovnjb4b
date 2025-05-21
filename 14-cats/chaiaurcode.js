d61194f09fc9aca5ba3ebfc293935939c8a72e78
Nikhil Shaw <Nikhilshaw575@gmail.com>
2025-05-21 21:33:01 +0530

    Update chaiaurcode.js
    Fix: Properly access image URL from API response array using index [0] and display it

--- 14-cats/chaiaurcode.js
+++ 14-cats/chaiaurcode.js
@@ -1,1 +1,21 @@
 const url = 'https://api.thecatapi.com/v1/images/search';
+const btn = document.querySelector(".btn");
+
+const imgContainer = document.createElement("div");
+document.body.appendChild(imgContainer);
+btn.addEventListener("click",()=>{
+  fetch(url)
+  .then(response=>response.json())
+  .then(data=>{
+    imgContainer.innerHTML="";
+    const img = document.createElement("img");
+    img.src=data[0].url;
+    img.style.width=data[0].width +"px";
+    img.style.height=data[0].height+"px";
+    imgContainer.appendChild(img)
+  })
+  .catch(error => {
+    console.error("Error fetching cat image:", error);
+  });
+
+})
